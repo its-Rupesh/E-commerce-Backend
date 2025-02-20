8eb905770b48026b5ebf6d0f1335561310f43e81
its-Rupesh <rupeshchimankare2004@gmail.com>
2025-02-20 11:34:19 +0530

    Multer Created File will Store in Upload folder. Later store it in Cloudinary

---
+++ src/middleware/multer.ts
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// Storage Created
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+// Multer for Single File
+const singleUpload = multer({ storage }).single("photo");
+
+export { singleUpload };
